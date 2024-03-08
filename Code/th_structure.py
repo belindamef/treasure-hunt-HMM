e041154aafe967c6c007add70572204ae6511be4
Belinda Fleischmann <fl.belinda@googlemail.com>
2024-03-08 16:48:40 +0100

    Add th_structure

---
+++ Code/th_structure.py
@@ -0,0 +1,13 @@
+class th_structure():
+    """This class definition file enables the Matlab structure-like use of 
+    Python objects.
+
+    Objects of type th_structure are initialized by
+        S       = th_structure
+
+    Attributes of th_structure type objects can be set as
+        S.att   = 'Hello world'
+
+   During interactive debugging, fields and values can be looked up by
+       S.__dict__
+    """
